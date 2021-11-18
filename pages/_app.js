422831c666b10730ee8dcc8e18951d159f292d31
SrBusta <brandonbustam1@gmail.com>
2021-11-18 18:27:04 -0500

    s15.9

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,11 @@ import '../styles/globals.css'
 import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }) {
-  return (<ThemeProvider attribute="class">
+  return (
+    //<ThemeProvider attribute="class">
     <Component {...pageProps} />
-  </ThemeProvider>)
+  // </ThemeProvider>
+  )
 
 
 }
