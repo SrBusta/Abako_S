fbf365ffa260b2b0a4304443ecc880aeea8500e8
SrBusta <brandonbustam1@gmail.com>
2021-11-23 18:49:15 -0500

    login

--- components/Login/LoginForm.js
+++ components/Login/LoginForm.js
@@ -45,8 +45,8 @@ function LoginForm() {
         const accessToken = await resJson.data.accessToken; // capturo el token que se envia desde el API
         const refreshToken = await resJson.data.refreshToken; // capturo el token que se envia desde el API
     
-        cookie.set("accessToken", accessToken, { expires: 5 / 24 }); // Seteo del cookie hacia la pagina
-        cookie.set("refreshToken", refreshToken, { expires: 5 / 24 }); // Seteo del cookie hacia la pagina
+       // cookie.set("accessToken", accessToken, { expires: 5 / 24 }); // Seteo del cookie hacia la pagina
+        //cookie.set("refreshToken", refreshToken, { expires: 5 / 24 }); // Seteo del cookie hacia la pagina
 
         if (resJson.error == false) {
             router.push('/dashboard')
